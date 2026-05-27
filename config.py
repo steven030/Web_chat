de689e0a0a15b5a95796e531c45745c06efb7f7f
Wannab1 <58341695+steven030@users.noreply.github.com>
2026-05-27 18:18:22 -0400

    Refactor Config class and add development settings
    Refactor Config class and add Is_development subclass with environment variables.

--- config.py
+++ config.py
@@ -3,16 +3,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-fallback-key")
+class Config(object):
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+class Is_development(Config):
+
+    PORT = os.getenv("PORT", 5000)
+    DEBUG = os.getenv("DEBUG", "False") == "True"
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # IMPORTANTE: en serverless NO uses rutas locales fijas
-    UPLOAD_FOLDER = "/tmp/uploads"
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    PORT = 5000
