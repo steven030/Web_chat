17a65d6b98c7c72f361735433538907cf0905205
Wannab1 <58341695+steven030@users.noreply.github.com>
2026-05-28 10:48:05 -0400

    Update chat_messages.js

--- static/js/chat_messages.js
+++ static/js/chat_messages.js
@@ -1,122 +1,72 @@
+// chat_messages.js
 const socket = io();
 
+socket.on('message', function(msg) {
+    const username = msg['username'];
+    let menssage = msg['message'];
+    let image = msg['img'];
+    let _alert = msg['alert'];
+    console.log(msg);
 
+    let mensaje = () => {
+        if (_alert == 'false') {
+            
+            // 1. Validamos en vivo si el mensaje proviene de ti mismo
+            const isMe = (username === window.currentUser);
+            
+            // 2. Asignamos la clase CSS correspondiente para Flexbox
+            const messageClass = isMe ? 'my-message' : 'other-message';
+            
+            // 3. Estructura HTML para el contenedor del avatar
+            const avatarHTML = `
+                <div class="chat-avatar-wrapper">
+                    <img class="user_images" src="static/source/uploads/${image}" alt="${username}">
+                </div>`;
+            
+            let htmlContent = '';
+            
+            // 4. Armamos los bloques respetando el orden visual exacto
+            if (isMe) {
+                // Si soy yo: Burbuja de texto primero, Avatar a la derecha
+                htmlContent = `
+                    <div class="chat-main ${messageClass} non-selectable">
+                        <div class="chat-content-body">
+                            <span class="chat-username">${username}</span>
+                            <li class="chat-box">
+                                <p id="content">${menssage}</p>
+                            </li>
+                        </div>
+                        ${avatarHTML}
+                    </div>`;
+            } else {
+                // Si es otra persona: Avatar a la izquierda, luego burbuja de texto
+                htmlContent = `
+                    <div class="chat-main ${messageClass} non-selectable">
+                        ${avatarHTML}
+                        <div class="chat-content-body">
+                            <span class="chat-username">${username}</span>
+                            <li class="chat-box">
+                                <p id="content">${menssage}</p>
+                            </li>
+                        </div>
+                    </div>`;
+            }
 
+            // 5. Inyectamos el nodo dinámico al final de la lista
+            $('#messages').append(htmlContent);
+            
+            // 6. Ejecutamos los efectos de sonido y scroll del archivo de estilos
+            setTimeout(sound('mensaje'), 300);
+            window.scrollDiv();
 
-
-
-socket.on('message',function(msg){
-
-
-	const username = msg['username'];
-	let menssage = msg['message'];
-	let image = msg['img']
-	let _alert = msg['alert']
-	console.log(msg)
-
-	let mensaje = ()=>{
-		if(_alert == 'false'){
-
-			$('#messages').append(`<div class="chat-main" class="non-selectable"> <img class="user_images" src="static/source/uploads/${image}" alt="${username}"> <p id="user"> ${username} </p> <li class ="chat-box"> ` +'<br> <p id="content">' + menssage + '</p> <br> </li> <br>');
-			setTimeout(sound('mensaje'),300);
-			var div = document.getElementById('sub-message');
-			div.scrollTop = '9999';
-
-
-		}else if(_alert == 'true'){
-			setTimeout(sound('notificacion'),300);
-			$('#messages').append(`<div id="content-notify" class="non-selectable"> <p id="notify">` + username + menssage + `</p></div>`);
-			var div = document.getElementById('sub-message');
-			div.scrollTop = '9999';
-		}
-	};
-
-	setTimeout(mensaje,50);
-
-
-
-
-
-
-
-	
-	
-});
-
-let sound = (param)=>{
-	if(param == 'mensaje'){
-		let etiquetaAudio = document.createElement("audio")
-		etiquetaAudio.setAttribute("src", "static/sounds/mensaje-sound.mp3")
-		etiquetaAudio.volume = 0.1;
-		etiquetaAudio.play()
-	}else if(param == 'notificacion'){
-		let etiquetaAudio = document.createElement("audio")
-		etiquetaAudio.setAttribute("src", "static/sounds/ALERT_29.mp3")
-		etiquetaAudio.volume = 0.1;
-		etiquetaAudio.play()
-	}
-
-}
-
-
-
-
-socket.on('connect',()=>{
-	
-})
-socket.on('disconnect',()=>{
-	console.log(socket.connected)
-	
-})
-socket.on('new_message',(data)=>{
-	alert(`nuevo mensaje ${data}`)
-})
-
-
-$(document).ready(function() {
-	var pulsado = false;
-    $("form").keydown(function(e) {
-
-    	if(pulsado) return false;
-        if (e.which === 13) {
-        	
-        	if($('#my_message').val() != '' && $('#my_message').val() != null ){
-           		socket.send($('#my_message').val());
-				$('#my_message').val(' ');
-			}
-			pulsado = true;
-			return false;
+        } else if (_alert == 'true') {
+            setTimeout(sound('notificacion'), 300);
+            $('#messages').append(`<div id="content-notify" class="non-selectable"><p id="notify">${username}${menssage}</p></div>`);
+            window.scrollDiv();
         }
-    })
-    .keyup(function(){
-		 pulsado = false;
-	});
+    };
+
+    setTimeout(mensaje, 50);
 });
 
-
-// $('#send').on('click', function(){
-// 	socket.send($('#my_message').val());
-// 	$('#my_message').val(' ');
-
-
-// });
-
-// $('#reply').on('click', function(){
-// 	socket.send($('#my_message').val());
-// 	$('#my_message').val(' ');
-
-// });
-
-window.onload = ()=>{
-
-	scrollDiv();
-
-
-}
-let scrollDiv = ()=>{
-
-    var div = document.getElementById('sub-message');
-    div.scrollTop = '1500';
- 
-};
-
+// Mantén aquí abajo tus funciones de reproducción de sound() y los listeners de keydown...
